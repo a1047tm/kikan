7ba3cbdc127c90f59730a3d9e82cf5722c56b73a
Atsuki Takahashi <a1133at@aiit.ac.jp>
2012-02-04 01:17:20 -0800

    Deploy

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "kikan"
+set :deploy_to, "/home/rails/kikan"
+
+set :repository,  "git@github.com:a1047tm/kikan"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :use_sudo, false
+ 
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+role :db,  "localhost"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kikan::Application.config.secret_token = '0fd3aec03c058bd0e1b9a2a42ec2cdce325f14fea8e32342e4324d450d115cc4e58469d426235caf7bba2bc09aa0b161e48f9545231fa883f27b15651a0bbe7e'
